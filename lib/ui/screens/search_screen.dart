82605c038f60d710a9696235cdcfb07d42ca5d66
pkm-sync <sync@houseofmates.space>
2026-08-15 13:56:09 -0700

    autosync: 2026-08-15T13:56:09

--- lib/ui/screens/search_screen.dart
+++ lib/ui/screens/search_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:watch/core/constants.dart';
-import 'package:watch/models/media_item.dart';
-import 'package:watch/ui/screens/player_screen.dart';
+import 'package:watch/services/providers.dart';
 import 'package:watch/ui/screens/image_viewer_screen.dart';
 import 'package:watch/services/providers.dart';
 
