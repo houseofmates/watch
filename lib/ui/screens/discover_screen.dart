fb43575faf163f3ef88c80ff8e4f75b11df67274
pkm-sync <sync@houseofmates.space>
2026-08-16 18:16:01 -0700

    autosync: 2026-08-16T18:16:01

--- lib/ui/screens/discover_screen.dart
+++ lib/ui/screens/discover_screen.dart
@@ -430,9 +430,9 @@ class _DetailSheet extends StatelessWidget {
                   child: ClipRRect(
                     borderRadius: BorderRadius.circular(8),
                     child: posterUrl != null
-                        ? (posterUrl.startsWith('http://') ||
-                            posterUrl.startsWith('https://') ||
-                            posterUrl.startsWith('/'))
+                        ? (posterUrl!.startsWith('http://') ||
+                            posterUrl!.startsWith('https://') ||
+                            posterUrl!.startsWith('/'))
                             ? Image.network(posterUrl!, fit: BoxFit.cover, errorBuilder: (_, __, ___) => placeholder)
                             : Image.file(File(posterUrl!), fit: BoxFit.cover, errorBuilder: (_, __, ___) => placeholder)
                         : placeholder,
